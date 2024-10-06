b025458d5f5af4e309c064b558e2990d0da003ca
￴￴ <>
2024-10-07 02:04:25 +0700

    add templates

---
+++ packages/isar/templates/server_project/bin/server_project.dart
@@ -0,0 +1,5 @@
+import 'package:server_project/server_project.dart' as server_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${server_project.calculate()}!');
+}
---
+++ packages/isar/templates/server_project/test/server_project_test.dart
@@ -0,0 +1,8 @@
+import 'package:server_project/server_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
